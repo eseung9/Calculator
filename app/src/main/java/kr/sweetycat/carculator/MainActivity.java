3057c4883f2ce935ec8e4e5d73ee35c67fdeebcb
Yi <eseung9@gmail.com>
2017-02-27 17:09:39 +0900

    Test commit

---
+++ app/src/main/java/kr/sweetycat/carculator/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.sweetycat.carculator;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
