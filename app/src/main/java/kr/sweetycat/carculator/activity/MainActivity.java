7dc113b0bc24d480a847bc65d11b47e6d87a7ee6
Yi <eseung9@gmail.com>
2017-02-28 22:36:11 +0900

    17/02/28

--- app/src/main/java/kr/sweetycat/carculator/activity/MainActivity.java
+++ app/src/main/java/kr/sweetycat/carculator/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package kr.sweetycat.carculator;
+package kr.sweetycat.carculator.activity;
 
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import kr.sweetycat.carculator.R;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        //Test
+
+        
+
     }
 }
---
+++ app/src/main/java/kr/sweetycat/carculator/common/Define.java
@@ -0,0 +1,13 @@
+package kr.sweetycat.carculator.common;
+
+/**
+ * Created by Yi on 2017-02-27.
+ */
+
+public class Define {
+
+    public Define(){
+
+    }
+
+}
